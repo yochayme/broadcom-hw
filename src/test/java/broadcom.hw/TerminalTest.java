bcf55f5b4fff961d55fc57bdb33c0003253a456e
ym662279 <yocahy.menachem@broadcom.com>
2021-02-15 12:34:47 +0200

    init

---
+++ src/test/java/broadcom.hw/TerminalTest.java
@@ -0,0 +1,15 @@
+package broadcom.hw;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TerminalTest {
+
+
+
+    @Test
+    public void testTerminal(){
+       assertEquals("Hello world", "Hello world");
+    }
+}
